e714683a34843cfdde43af40a6e4fef542fc5bea
Taimoor Ahmad <taimooraug@gmail.com>
2019-10-12 07:31:02 -0400

    Formatting code: tabs = 4 spaces

--- src/App.js
+++ src/App.js
@@ -1,124 +1,127 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Task from './Task'
-import {DragDropContext} from 'react-beautiful-dnd'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 class App extends React.Component {
 
-  /* 
-  A tab of n means:
-  0 -> backlog
-  1 -> TODO
-  2 -> Doing
-  3 -> Done!
-  */
-  state = {
-    entry: "",
-    tasks: [
-      {
-        text: "Brush your teeth", 
-        tab: 0,
-      },
-      {
-        text: "Charge your phone", 
-        tab: 0,
-      },
-      {
-        text: "Clean the dishes",
-        tab: 0,
-      },
+	/* 
+	A tab of n means:
+	0 -> backlog
+	1 -> TODO
+	2 -> Doing
+	3 -> Done!
+	*/
+	state = {
+		entry: "",
+		tasks: [
+			{
+				text: "Brush your teeth",
+				tab: 0,
+			},
+			{
+				text: "Charge your phone",
+				tab: 0,
+			},
+			{
+				text: "Clean the dishes",
+				tab: 0,
+			},
 
-      {
-        text: "Make a review of the Scrimba Course",
-        tab: 1,
-      },
+			{
+				text: "Make a review of the Scrimba Course",
+				tab: 1,
+			},
 
-      {
-        text: "Learn React",
-        tab: 2,
-      },
-      {
-        text: "Become job ready",
-        tab: 2,
-      },
+			{
+				text: "Learn React",
+				tab: 2,
+			},
+			{
+				text: "Become job ready",
+				tab: 2,
+			},
 
-      {
-        text: "Write a resume",
-        tab: 3,
-      }
-    ]
-  }
+			{
+				text: "Write a resume",
+				tab: 3,
+			}
+		]
+	}
 
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.createTask = this.createTask.bind(this)
-    this.renderColumn = this.renderColumn.bind(this)
-  }
+		this.createTask = this.createTask.bind(this)
+		this.renderColumn = this.renderColumn.bind(this)
+	}
 
-  createTask() {
-    console.log(this.state.entry)
+	createTask() {
+		console.log(this.state.entry)
 
-    if (this.state.entry.trim() !== "") {
+		if (this.state.entry.trim() !== "") {
 
-      this.setState(prevState => ({
-        tasks: [...prevState.tasks, {
-          text: this.state.entry.trim(),
-          tab: 0
-        }]
-      }))
-    }
+			this.setState(prevState => ({
+				tasks: [...prevState.tasks, {
+					text: this.state.entry.trim(),
+					tab: 0
+				}]
+			}))
+		}
 
-    this.setState(prevState => ({
-      entry: ""
-    }))
-  }
+		this.setState(prevState => ({
+			entry: ""
+		}))
+	}
 
-  renderColumn(col) {
-    return <div>
-      {
-        this.state.tasks.map((task) => (
-          <span style={{display: task.tab === col ? "block" : "none"}}><Task text={task.text} /></span>
-        ))
-      }
-    </div>
-  }
+	renderColumn(col) {
+		return <div>
+			{
+				this.state.tasks.map((task) => (
+					<span style={{ display: task.tab === col ? "block" : "none" }}><Task text={task.text} /></span>
+				))
+			}
+		</div>
+	}
 
-  onDragEnd = result => {
-    
-  }
+	render() {
+		return <div>
+			<DragDropContext>
+				<Droppable>
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<h1>Backlog</h1>
 
-  render() {
-    return <div>
-      <DragDropContext onDragEnd={this.onDragEnd}>
-      <div class="container">
-        <div class="row">
-            <div class="col">
-              <h1>Backlog</h1>
-              {this.renderColumn(0)}
-              <div>
-                <input value={this.state.entry} type="text" name="input_text" onChange={event => this.setState({entry: event.target.value})} />
-                <button onClick={this.createTask}>Insert</button>
-              </div>
-            </div>
-            <div class="col">
-              <h1>TODO</h1>
-              {this.renderColumn(1)}
-            </div>
-            <div class="col">
-              <h1>In progress</h1>
-              {this.renderColumn(2)}
-            </div>
-            <div class="col">
-              <h1>Done!</h1>
-                {this.renderColumn(3)}
-            </div>
-        </div>
-    </div>
-    </DragDropContext>
-    </div>
-  }
+								{this.renderColumn(0)}
+
+								<div>
+									<input value={this.state.entry} type="text" name="input_text" onChange={event => this.setState({ entry: event.target.value })} />
+									<button onClick={this.createTask}>Insert</button>
+								</div>
+							</div>
+							<div class="col">
+								<h1>TODO</h1>
+
+								{this.renderColumn(1)}
+							</div>
+							<div class="col">
+								<h1>In progress</h1>
+
+								{this.renderColumn(2)}
+							</div>
+							<div class="col">
+								<h1>Done!</h1>
+								
+								{this.renderColumn(3)}
+							</div>
+						</div>
+					</div>
+				</Droppable>
+			</DragDropContext>
+		</div>
+	}
 }
 
 export default App;
--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Draggable } from 'react-beautiful-dnd'
 
 class Task extends React.Component {
-    render() {
-        return <div>
-            <h5><span role="img" class="drag_icon">🗿</span> {this.props.text}</h5>
-        </div>
-    }
+	render() {
+		return <div><Draggable>
+			<h5><span role="img" class="drag_icon">🗿</span>{this.props.text}</h5>
+		</Draggable></div>
+	}
 }
 
 export default Task;
