27ff62607583329aa64489181506911815354880
Taimoor Ahmad <taimooraug@gmail.com>
2019-10-14 16:54:27 -0400

    Working on re-doing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Task from './Task';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
-import TestData from './TestData';
-
-
-function getRandomId() {
-	let min = Math.ceil(1);
-	let max = Math.floor(Number.MAX_SAFE_INTEGER);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 class App extends React.Component {
 
@@ -28,23 +20,19 @@ class App extends React.Component {
 	createTask() {
 		console.log(this.state.entry)
 
+		// Insert to backlog
 		if (this.state.entry.trim() !== "") {
-
-			this.setState(prevState => ({
-				tasks: [...prevState.tasks, {
-					id: getRandomId(),
-					text: this.state.entry.trim(),
-				}]
-			}))
+			
 		}
 
+		// Clear
 		this.setState(prevState => ({
 			entry: ""
 		}))
 	}
 
 	onDragEnd = result => {
-		// TODO.
+		// Dragging logic
 	}
 
 	render() {
@@ -55,11 +43,9 @@ class App extends React.Component {
 						<Droppable droppableId="backlogDroppable">
 							{(provided) => (
 								<div class="col" ref={provided.innerRef} {...provided.droppableProps}>
-									<h1>💭 Backlog</h1>
+									// Title of board
 
-									{this.state.tasks.map((task, index) => (
-										<span style={{ display: task.tab === BACKLOG ? "block" : "none" }}><Task task={task} index={index} /></span>
-									))}
+									// Tasks of board
 
 									{provided.placeholder}
 
@@ -67,45 +53,6 @@ class App extends React.Component {
 										<input value={this.state.entry} type="text" name="input_text" onChange={event => this.setState({ entry: event.target.value })} />
 										<button onClick={this.createTask}>Insert</button>
 									</div>
-								</div>
-							)}
-						</Droppable>
-						<Droppable droppableId="todoDroppable">
-							{(provided) => (
-								<div class="col" ref={provided.innerRef} {...provided.droppableProps}>
-									<h1>👨‍🏭 TODO</h1>
-
-									{this.state.tasks.map((task, index) => (
-										<span style={{ display: task.tab === TODO ? "block" : "none" }}><Task task={task} index={index} /></span>
-									))}
-
-									{provided.placeholder}
-								</div>
-							)}
-						</Droppable>
-						<Droppable droppableId="doingDroppable">
-							{(provided) => (
-								<div class="col" ref={provided.innerRef} {...provided.droppableProps}>
-									<h1>💪 Crushing</h1>
-
-									{this.state.tasks.map((task, index) => (
-										<span style={{ display: task.tab === DOING ? "block" : "none" }}><Task task={task} index={index} /></span>
-									))}
-
-									{provided.placeholder}
-								</div>
-							)}
-						</Droppable>
-						<Droppable droppableId="doneDroppable">
-							{(provided) => (
-								<div class="col" ref={provided.innerRef} {...provided.droppableProps}>
-									<h1>🙌 Crushed</h1>
-
-									{this.state.tasks.map((task, index) => (
-										<span style={{ display: task.tab === DONE ? "block" : "none" }}><Task task={task} index={index} /></span>
-									))}
-
-									{provided.placeholder}
 								</div>
 							)}
 						</Droppable>
